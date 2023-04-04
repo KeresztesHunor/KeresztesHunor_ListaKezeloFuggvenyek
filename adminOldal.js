3e636d30a717030f99bacd5dbdde58e34d6045d8
KeresztesHunor <kereszteshunor36gmail.com>
2023-04-04 18:19:34 +0200

    összes szűrési lehetőség használható egyszerre

--- adminOldal.js
+++ adminOldal.js
@@ -71,7 +71,7 @@ $(() =>
             {
                 let txt = "";
                 txt += ujTagekKozeIr("a", "href='#' class='text-light text-decoration-none'", KULCS_NEVEK[kulcs]);
-                switch (typeof(OBJEKTUM_LISTA[0][KULCSOK_LISTA[index]]))
+                switch (typeof (OBJEKTUM_LISTA[0][KULCSOK_LISTA[index]]))
                 {
                     case "number":
                         txt += ujTagekKozeIr("div", null, (() =>
@@ -103,44 +103,67 @@ $(() =>
     //Szűrési lehetőség inicializálása
 
     const AUTOK_TABLA_INPUT_MEZOK = $("#autok > table > thead > tr > th input").toArray();
-    AUTOK_TABLA_INPUT_MEZOK.forEach(inputMezo =>
+    const INPUT_MEZOK_SZURESI_FUGGVENYEK = (() =>
     {
-        const INPUT_MEZO_TIPUS = $(inputMezo).attr("type");
-        switch (INPUT_MEZO_TIPUS)
+        const OBJEKTUM = {};
+        AUTOK_TABLA_INPUT_MEZOK.forEach(inputMezo =>
         {
-            case "text":
-                szuresInit(inputMezo, kulcs => objektum => objektum[kulcs].includes($(inputMezo).val()));
-                break;
-            case "number":
-                szuresInit(inputMezo, kulcs =>
+            OBJEKTUM[$(inputMezo).attr("id").slice(1).toLowerCase()] = (() =>
+            {
+                const INPUT_MEZO_TIPUS = $(inputMezo).attr("type");
+                switch (INPUT_MEZO_TIPUS)
                 {
-                    const MEZO_ERTEK = $(inputMezo).val();
-                    if (MEZO_ERTEK.length > 0)
-                    {
-                        switch ($(inputMezo).siblings("select").first().val())
+                    case "text":
+                        return objektumErtek => objektumErtek.includes($(inputMezo).val());
+                    case "number":
+                        return objektumErtek =>
                         {
-                            case "kisebb":
-                                return objektum => objektum[kulcs] < MEZO_ERTEK;
-                            case "nagyobb":
-                                return objektum => objektum[kulcs] > MEZO_ERTEK;
-                            case "kisebbEgyenlo":
-                                return objektum => objektum[kulcs] <= MEZO_ERTEK;
-                            case "nagyobbEgyenlo":
-                                return objektum => objektum[kulcs] >= MEZO_ERTEK;
-                            default:
-                                return objektum => objektum[kulcs] == MEZO_ERTEK; //Ide "==" összehasonlítás kell "===" helyett mert egy "number" típusú input field is "string"-gel tér vissza és túl lusta vagyok átkonvertálni számra
+                            const MEZO_ERTEK = $(inputMezo).val();
+                            if (MEZO_ERTEK.length > 0)
+                            {
+                                const MEZO_SZAM_ERTEK = parseInt(MEZO_ERTEK);
+                                switch ($(inputMezo).siblings("select").first().val())
+                                {
+                                    case "kisebb":
+                                        return objektumErtek < MEZO_SZAM_ERTEK;
+                                    case "nagyobb":
+                                        return objektumErtek > MEZO_SZAM_ERTEK;
+                                    case "kisebbEgyenlo":
+                                        return objektumErtek <= MEZO_SZAM_ERTEK;
+                                    case "nagyobbEgyenlo":
+                                        return objektumErtek >= MEZO_SZAM_ERTEK;
+                                    default:
+                                        return objektumErtek === MEZO_SZAM_ERTEK;
+                                }
+                            }
+                            else
+                            {
+                                return true; //Ha a mező üres, akkor mindent írjunk ki
+                            }
                         }
-                    }
-                    else
-                    {
-                        return objektum => true; //Ha a mező üres, akkor mindent írjunk ki
-                    }
-                });
-                break;
-            default:
-                console.log(`Hiba! Ilyen input típusra nincs szűrési lehetőség (${INPUT_MEZO_TIPUS}).`);
-                break;
-        }
+                    default:
+                        console.log(`Hiba! Ilyen input típusra nincs szűrési lehetőség (${INPUT_MEZO_TIPUS}).`);
+                        return objektumErtek => {};
+                }
+            })()
+        });
+        return OBJEKTUM;
+    })();
+    const INPUT_ELEMEK = $("#autok > table > thead > tr > th").find("input, select");
+    $(INPUT_ELEMEK).on("input", () =>
+    {
+        valtoztathatoObjektumLista = szures(OBJEKTUM_LISTA, objektum =>
+        {
+            for (const KULCS in INPUT_MEZOK_SZURESI_FUGGVENYEK)
+            {
+                if (!INPUT_MEZOK_SZURESI_FUGGVENYEK[KULCS](objektum[KULCS]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        });
+        tablazatotKiir(autokTablaBody, valtoztathatoObjektumLista);
     });
 
     //Új autó felvitelének lehetőségének inicializálása
@@ -220,22 +243,13 @@ function tablazatotKiir(szuloElem, lista)
 
 function ujInputMezotIr(kulcs, tipus)
 {
+    const NAME_ES_PLACEHOLDER = KULCS_NEVEK[kulcs].toLowerCase();
     return ujParatlanTagetIr("input", `
         id="${"i" + kulcs.charAt(0).toUpperCase() + kulcs.slice(1)}"
         type="${tipus}"
-        placeholder="${KULCS_NEVEK[kulcs]}"
-        name="${KULCS_NEVEK[kulcs]}"
+        placeholder="${NAME_ES_PLACEHOLDER}"
+        name="${NAME_ES_PLACEHOLDER}"
     `);
-}
-
-function szuresInit(inputMezo, feltetel)
-{
-    const KULCS = $(inputMezo).attr("id").slice(1).toLowerCase();
-    $(inputMezo).on("input", () =>
-    {
-        valtoztathatoObjektumLista = szures(OBJEKTUM_LISTA, feltetel(KULCS));
-        tablazatotKiir(autokTablaBody, valtoztathatoObjektumLista);
-    });
 }
 
 function helyesAdatok(inputMezok)
